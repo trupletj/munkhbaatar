7f18c6bc00ecee5c83c9f1ea56a4edde516a6bf5
trupletj <tuvshinjargal01@gmail.com>
2022-03-01 20:29:42 +0800

    shine update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,11 @@
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bteg: "#fd6a02",
+      },
+    },
   },
   plugins: [],
-}
+};
